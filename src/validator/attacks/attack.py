e106b6734a7dd10f601fd93b5c7b20e00ef7940f
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-02-09 15:03:33 -0800

    Rust implementation of s_prime

--- src/validator/attacks/attack.py
+++ src/validator/attacks/attack.py
@@ -4,6 +4,8 @@ except Exception:
     retrieve_rs = None
 import argparse
 import h5py
+from ..parameters import *
+import ast
 
 
 def _linearization(ciphertext_file, public_key_file):
@@ -20,26 +22,29 @@ def main():
     parser.add_argument("i", type=int)
     args = parser.parse_args()
 
-    if retrieve_rs is None:
-        raise RuntimeError(
-            """`retrieve_rs` module not built. 
-            Run `source venv/bin/activate && maturin develop --manifest-path src/validator/attacks/retrieve_rs/Cargo.toml`"""
-        )
-    
-    ct = [[2, 3, 4], [6], [5, 5], [7, 3]]
-    pk = [[(1, 1), (2, 0), (3, 1)], [(2, 0), (3, 1), (4, 0)], [(4, 0), (5, 1), (6, 1)], [(6, 1), (1, 1), (3, 0)]]
-    n = 10
-    res = retrieve_rs.retrieve(ct, pk, n)
-    print(res)
+    DIR = f"tests/c_{args.i}"
 
-    CIPHERTEXT_DIRPATH = f"tests/c_{args.i}"
-    CIPHERTEXT_FILEPATH = f"{CIPHERTEXT_DIRPATH}/ciphertext_{args.i}.hdf5"
-    CLAUSES_FILEPATH = f"{CIPHERTEXT_DIRPATH}/public_key_{args.i}.txt"
+    with h5py.File(f"{DIR}/ciphertext_{args.i}.hdf5", "r") as CIPHERTEXT_FILE:
+        with open(f"{DIR}/public_key_{args.i}.txt", "r") as PUBLIC_KEY_FILE:
+            if retrieve_rs is None:
+                raise RuntimeError(
+                    """`retrieve_rs` module not built. 
+                    Run `source venv/bin/activate && maturin develop --manifest-path src/validator/attacks/retrieve_rs/Cargo.toml`"""
+                )
 
-    with h5py.File(CIPHERTEXT_FILEPATH, "r") as CIPHERTEXT_FILE:
-        with open(CLAUSES_FILEPATH, "r") as CLAUSES_FILE:
-            y = _linearization(CIPHERTEXT_FILE, CLAUSES_FILE)
-            print(y)
+            # ct = [[2, 3, 4], [6], [5, 5], [7, 3]]
+            # pk = [[(1, 1), (2, 0), (3, 1)], [(2, 0), (3, 1), (4, 0)], [(4, 0), (5, 1), (6, 1)], [(6, 1), (1, 1), (3, 0)]]
+            # n = 10
+
+            ct = list({tuple(m) for m in CIPHERTEXT_FILE["ciphertext"]})
+            pk = [tuple(c) for c in ast.literal_eval(PUBLIC_KEY_FILE.read())]
+            n = N
+
+            res = retrieve_rs.retrieve(ct, pk, n)
+            for x in res:
+                print(x.clause, x.vars)
+            # y = _linearization(CIPHERTEXT_FILE, CLAUSES_FILE)
+            # print(y)
 
 
 if __name__ == "__main__":
--- src/validator/attacks/retrieve_rs/src/lib.rs
+++ src/validator/attacks/retrieve_rs/src/lib.rs
@@ -1,26 +1,36 @@
 use pyo3::prelude::*;
 use std::collections::{HashMap, HashSet};
 
+#[pyclass]
+#[derive(Clone)]
+pub struct FormattedElement {
+    #[pyo3(get)]
+    clause: Vec<(u64, u8)>,
+    #[pyo3(get)]
+    vars: u128,
+}
+
 #[pyfunction]
 pub fn retrieve(
-    ciphertext: Vec<Vec<u128>>,
-    public_key: Vec<Vec<Vec<u64>>>,
+    ciphertext: Vec<Vec<u64>>,
+    public_key: Vec<Vec<(u64, u8)>>,
     n: u128,
-) -> PyResult<HashMap<u64, Vec<Vec<u64>>>> {
+    // ) -> PyResult<HashMap<u64, Vec<Vec<u64>>>> {
+) -> PyResult<Vec<FormattedElement>> {
     // ================================
     //      Step 1
     // ================================
     if n > 128 {
         panic!("`N` was {}, must be <= 128", n);
     }
-    let mut var_to_bit = HashMap::new();
-    let mut bit_to_var = HashMap::new();
-    for i in 0..n {
+    let mut var_to_bit: HashMap<u64, u128> = HashMap::new();
+    let mut bit_to_var: HashMap<u128, u64> = HashMap::new();
+    for i in 0..n as u64 {
         let v: u128 = 1 << i;
         var_to_bit.insert(i + 2, v);
         bit_to_var.insert(v, i + 2);
     }
-    println!("v2b {:?}", var_to_bit);
+    // println!("v2b {:?}", var_to_bit);
 
     let mut unique_masks: HashSet<u128> = HashSet::new();
     for m in ciphertext {
@@ -33,36 +43,58 @@ pub fn retrieve(
 
     let unique_masks_vec: Vec<u128> = unique_masks.into_iter().collect();
 
-    let mut combination_masks: HashSet<u128> = HashSet::new();
+    let mut s_combination_masks: HashSet<u128> = HashSet::new();
     for (i, mask_a) in unique_masks_vec.iter().enumerate() {
         for mask_b in &unique_masks_vec[i + 1..] {
-            combination_masks.insert(mask_a | mask_b);
+            s_combination_masks.insert(mask_a | mask_b);
         }
     }
+
     // ================================
     //      Step 2
     // ================================
 
-    let mut public_key_vars_only: Vec<Vec<u64>> = Vec::new();
+    let mut public_key_formatted: Vec<FormattedElement> = Vec::new();
     for c in public_key {
-        let mut clause_vars: Vec<u64> = Vec::new();
-        for v in c {
-            clause_vars.push(v[0].clone());
+        let mut vars_extracted: u128 = 0;
+        for v in &c {
+            vars_extracted |= var_to_bit[&v.0];
         }
-        public_key_vars_only.push(clause_vars);
+        let e = FormattedElement {
+            clause: c.clone(),
+            vars: vars_extracted,
+        };
+        public_key_formatted.push(e);
     }
 
-    let mut clauses_sharing_variable: HashMap<u64, Vec<Vec<u64>>> = HashMap::new();
+    let mut clauses_sharing_variable: HashMap<u64, (Vec<FormattedElement>, u128)> = HashMap::new();
     for i in 2..(n as u64) + 2 {
+        let clauses_vec: Vec<FormattedElement> = public_key_formatted
+                .iter()
+                .filter(|c| c.vars & (var_to_bit[&i]) > 0)
+                .cloned()
+                .collect();
         clauses_sharing_variable.insert(
             i,
-            public_key_vars_only
-                .iter()
-                .filter(|c| c.contains(&i))
-                .cloned()
-                .collect(),
+            (clauses_vec.clone(), clauses_vec.into_iter().fold(0, |var_set, x| var_set | x.vars))
         );
     }
+
+    let mut s_prime_expanded_from_s: HashSet<u128> = HashSet::new();
+    for s_i in s_combination_masks {
+        // println!("HEY {:b}", s_i);
+        let mut b = s_i.clone();
+        while b != 0 {
+            let var: u64 = b.trailing_zeros().into();
+            b &= b - 1;
+            s_prime_expanded_from_s.insert(s_i | clauses_sharing_variable[&var].1);
+            }
+        }
+    
+    for x in s_prime_expanded_from_s {
+        println!("s_prime_i {}", x);
+    }
+    
 
     // ================================
     //      Step 3
@@ -71,11 +103,14 @@ pub fn retrieve(
     // ================================
     //      Step 4
     // ================================
-    Ok(clauses_sharing_variable)
+
+    Ok(public_key_formatted)
+
 }
 
 #[pymodule]
 fn retrieve_rs(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<FormattedElement>()?;
     m.add_function(wrap_pyfunction!(retrieve, m)?)?;
     Ok(())
 }
