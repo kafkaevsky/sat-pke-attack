46aa03d3a0c8c4de42c40bfb902b7ae4a055c6b9
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-02-10 14:53:29 -0800

    cnf to neg anf, Rust migration

--- src/validator/attacks/retrieve_rs/src/lib.rs
+++ src/validator/attacks/retrieve_rs/src/lib.rs
@@ -1,11 +1,12 @@
+use itertools::Itertools;
 use pyo3::prelude::*;
 use std::collections::{HashMap, HashSet};
 
 #[pyclass]
-#[derive(Clone)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct FormattedElement {
     #[pyo3(get)]
-    clause: Vec<(u64, u8)>,
+    clause: Vec<(u64, u64)>,
     #[pyo3(get)]
     vars: u128,
 }
@@ -13,7 +14,7 @@ pub struct FormattedElement {
 #[pyfunction]
 pub fn retrieve(
     ciphertext: Vec<Vec<u64>>,
-    public_key: Vec<Vec<(u64, u8)>>,
+    public_key: Vec<Vec<(u64, u64)>>,
     n: u128,
     // ) -> PyResult<HashMap<u64, Vec<Vec<u64>>>> {
 ) -> PyResult<Vec<FormattedElement>> {
@@ -70,13 +71,18 @@ pub fn retrieve(
     let mut clauses_sharing_variable: HashMap<u64, (Vec<FormattedElement>, u128)> = HashMap::new();
     for i in 2..(n as u64) + 2 {
         let clauses_vec: Vec<FormattedElement> = public_key_formatted
-                .iter()
-                .filter(|c| c.vars & (var_to_bit[&i]) > 0)
-                .cloned()
-                .collect();
+            .iter()
+            .filter(|c| c.vars & (var_to_bit[&i]) > 0)
+            .cloned()
+            .collect();
         clauses_sharing_variable.insert(
             i,
-            (clauses_vec.clone(), clauses_vec.into_iter().fold(0, |var_set, x| var_set | x.vars))
+            (
+                clauses_vec.clone(),
+                clauses_vec
+                    .into_iter()
+                    .fold(0, |var_set, x| var_set | x.vars),
+            ),
         );
     }
 
@@ -88,24 +94,64 @@ pub fn retrieve(
             let var: u64 = b.trailing_zeros().into();
             b &= b - 1;
             s_prime_expanded_from_s.insert(s_i | clauses_sharing_variable[&var].1);
-            }
         }
-    
-    for x in s_prime_expanded_from_s {
-        println!("s_prime_i {}", x);
     }
-    
 
     // ================================
     //      Step 3
     // ================================
+
+    let mut t_contained_by_clauses: Vec<Vec<Vec<(u64, u64)>>> = Vec::new();
+    for s_prime_i in s_prime_expanded_from_s {
+        let mut t_i_hashset = HashSet::new();
+        let mut b = s_prime_i.clone();
+        while b != 0 {
+            let var: u64 = b.trailing_zeros().into();
+            b &= b - 1;
+            for candidate_clause in &clauses_sharing_variable[&var].0 {
+                if (candidate_clause.vars & !s_prime_i) == 0 {
+                    // vars in candidate_clause are contained by vars in s_prime_i
+                    t_i_hashset.insert(candidate_clause.clause.clone());
+                }
+            }
+        }
+
+        let t_i: Vec<Vec<(u64, u64)>> = Vec::from_iter(t_i_hashset);
+        if t_i.len() > 0 {
+            t_contained_by_clauses.push(t_i.clone());
+        }
+    }
+
+    fn cnf_to_neg_anf(clause: &Vec<(u64, u64)>) -> Vec<Vec<u64>> {
+        println!("========");
+        let mut clause_iterable_negated: Vec<Vec<u64>> = vec![vec![1]];
+        for m in clause {
+            clause_iterable_negated.push(vec![m.0, m.1]);
+        }
+
+        let mut neg_anf_simplified: Vec<Vec<u64>> = Vec::new();
+        for m in clause_iterable_negated
+            .iter()
+            .map(|v| v.iter().copied())
+            .multi_cartesian_product()
+        {
+            if !m.contains(&0) {
+                neg_anf_simplified.push(m.into_iter().filter(|v| *v != 1 as u64).collect());
+            }
+        }
+        neg_anf_simplified
+    }
+
+    for t_i in t_contained_by_clauses {
+        let c_1 = &t_i[0];
+        let mut anf_c_1: Vec<Vec<u64>> = cnf_to_neg_anf(&c_1);
+    }
 
     // ================================
     //      Step 4
     // ================================
 
     Ok(public_key_formatted)
-
 }
 
 #[pymodule]
