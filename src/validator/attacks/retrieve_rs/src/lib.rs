8001f9dec55ba0ed7098ab7163dcc034f930e5c3
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-02-12 16:04:40 -0800

    more optimizations

--- src/validator/attacks/retrieve_rs/src/lib.rs
+++ src/validator/attacks/retrieve_rs/src/lib.rs
@@ -1,6 +1,4 @@
-use itertools::Itertools;
 use pyo3::prelude::*;
-use rand::seq::SliceRandom;
 use rand::RngExt;
 use rayon::prelude::*;
 use std::collections::{HashMap, HashSet};
@@ -18,34 +16,42 @@ const INDEX_0_OFFSET: u64 = 2;
 const MAX_SAMPLES: usize = 100;
 const SAMPLE_THRESHOLD: usize = 30;
 
-fn encode_bit_vector(m: Vec<u64>) -> u128 {
-    let mut b: u128 = 0b0;
-    for v in m {
-        b |= 1u128 << (v - INDEX_0_OFFSET);
-    }
-    b
+#[inline(always)]
+fn encode_bit_vector(var: u64) -> u128 {
+    1u128 << (var - INDEX_0_OFFSET)
 }
 
 fn decode_bit_vector(v: u128) -> Vec<u64> {
-    let mut m: Vec<u64> = Vec::new();
-    for i in 0..128 {
-        if 1 << i & v != 0 {
-            m.push(i + INDEX_0_OFFSET);
-        }
+    let mut m: Vec<u64> = Vec::with_capacity(v.count_ones() as usize);
+    let mut bits = v;
+    while bits != 0 {
+        let i = bits.trailing_zeros() as u64;
+        m.push(i + INDEX_0_OFFSET);
+        bits &= bits - 1;
     }
     m
 }
 
-fn set_union(a: u128, b: u128) -> u128 {
-    return a | b;
-}
-
-fn set_intersection(a: u128, b: u128) -> u128 {
-    return a & b;
-}
-
-fn set_issubset(a: u128, b: u128) -> bool {
-    return a & !b == 0;
+fn cnf_to_neg_anf(clause: &Vec<(u64, u64)>) -> Vec<u128> {
+    let mut result: HashSet<u128> = HashSet::new();
+    result.insert(0);
+    for &(var, sign) in clause {
+        let encoded_var = encode_bit_vector(var);
+        let mut updated_result: HashSet<u128> = HashSet::new();
+        for &m in &result {
+            if !updated_result.remove(&m) {
+                updated_result.insert(m);
+            }
+            if sign != 0 {
+                let m_with_var = m | encoded_var;
+                if !updated_result.remove(&m_with_var) {
+                    updated_result.insert(m_with_var);
+                }
+            }
+        }
+        result = updated_result;
+    }
+    result.into_iter().collect()
 }
 
 #[pyfunction]
@@ -53,7 +59,7 @@ pub fn retrieve(
     ciphertext: Vec<Vec<u64>>,
     public_key: Vec<Vec<(u64, u64)>>,
     n: u128,
-) -> PyResult<Vec<FormattedElement>> {
+) -> PyResult<Vec<Vec<FormattedElement>>> {
     // ================================
     //      Step 1
     // ================================
@@ -61,19 +67,11 @@ pub fn retrieve(
         panic!("`N` was {}, must be <= 128", n);
     }
 
-    let mut var_to_bit: HashMap<u64, u128> = HashMap::with_capacity(n as usize);
-    let mut bit_to_var: HashMap<u128, u64> = HashMap::with_capacity(n as usize);
-    for i in 2..(n as u64) + 2 {
-        let encoding: u128 = encode_bit_vector(vec![i]);
-        var_to_bit.insert(i, encoding);
-        bit_to_var.insert(encoding, i);
-    }
-
     let mut unique_masks: HashSet<u128> = HashSet::with_capacity(ciphertext.len());
     for m in ciphertext {
         let mut mask: u128 = 0b0;
         for v in m {
-            mask |= var_to_bit[&v];
+            mask |= encode_bit_vector(v);
         }
         unique_masks.insert(mask);
     }
@@ -87,7 +85,7 @@ pub fn retrieve(
         .flat_map(|i| {
             unique_masks_vec[i + 1..]
                 .iter()
-                .map(|mask_b| unique_masks_vec[i] | mask_b)
+                .map({let value = unique_masks_vec.clone(); move |mask_b| value[i] | mask_b})
                 .collect::<Vec<_>>()
         })
         .collect();
@@ -99,30 +97,32 @@ pub fn retrieve(
     // ================================
 
     let mut public_key_formatted: Vec<FormattedElement> = Vec::with_capacity(public_key.len());
-    for c in public_key {
+    for c in &public_key {
         let mut vars_extracted: u128 = 0;
-        for v in &c {
-            vars_extracted |= var_to_bit[&v.0];
+        for v in c {
+            vars_extracted |= encode_bit_vector(v.0);
         }
-        let e = FormattedElement {
+        public_key_formatted.push(FormattedElement {
             clause: c.clone(),
             vars: vars_extracted,
-        };
-        public_key_formatted.push(e);
+        });
     }
 
     let mut clauses_sharing_variable: HashMap<u64, (Vec<FormattedElement>, u128)> = HashMap::new();
     for i in 2..(n as u64) + 2 {
+        let encoded_var = encode_bit_vector(i);
         let mut union_vars: u128 = 0;
         let mut clauses_vec: Vec<FormattedElement> = Vec::new();
         for c in &public_key_formatted {
-            if c.vars & var_to_bit[&i] != 0 {
+            if c.vars & encoded_var != 0 {
                 union_vars |= c.vars;
                 clauses_vec.push(c.clone());
             }
         }
         clauses_sharing_variable.insert(i, (clauses_vec, union_vars));
     }
+
+    eprintln!("clauses_sharing_variable: {}", clauses_sharing_variable.len());
 
     let s_prime_expanded_from_s: HashSet<u128> = s_combination_masks
         .par_iter()
@@ -144,26 +144,35 @@ pub fn retrieve(
     //      Step 3
     // ================================
 
-    let mut t_contained_by_clauses: Vec<Vec<FormattedElement>> = Vec::new();
-    for s_prime_i in s_prime_expanded_from_s {
-        let mut t_i_hashset = HashSet::new();
-        let mut b = s_prime_i.clone();
-        while b != 0 {
-            let var: u64 = (b.trailing_zeros() as u64) + INDEX_0_OFFSET;
-            b &= b - 1;
-            for candidate_clause in &clauses_sharing_variable[&var].0 {
-                if (candidate_clause.vars & !s_prime_i) == 0 {
-                    // vars in candidate_clause are contained by vars in s_prime_i
-                    t_i_hashset.insert(candidate_clause.clone());
+    let s_prime_vec: Vec<u128> = s_prime_expanded_from_s.into_iter().collect();
+
+    let t_contained_by_clauses: Vec<Vec<FormattedElement>> = s_prime_vec
+        .par_iter()
+        .filter_map(|&s_prime_i| {
+            let mut t_i_hashset: HashSet<*const FormattedElement> = HashSet::new();
+            let mut t_i: Vec<FormattedElement> = Vec::new();
+            let mut b = s_prime_i;
+            while b != 0 {
+                let var = (b.trailing_zeros() as u64) + INDEX_0_OFFSET;
+                b &= b - 1;
+                if let Some((clauses, _)) = clauses_sharing_variable.get(&var) {
+                    for candidate_clause in clauses {
+                        if (candidate_clause.vars & !s_prime_i) == 0 {
+                            let ptr = candidate_clause as *const FormattedElement;
+                            if t_i_hashset.insert(ptr) {
+                                t_i.push(candidate_clause.clone());
+                            }
+                        }
+                    }
                 }
             }
-        }
-
-        let t_i: Vec<FormattedElement> = Vec::from_iter(t_i_hashset);
-        if t_i.len() > 0 {
-            t_contained_by_clauses.push(t_i.clone());
-        }
-    }
+            if t_i.is_empty() {
+                None
+            } else {
+                Some(t_i)
+            }
+        })
+        .collect();
 
     eprintln!("t_contained_by_clauses: {}", t_contained_by_clauses.len());
 
@@ -171,77 +180,54 @@ pub fn retrieve(
     //      Step 4
     // ================================
 
-    fn cnf_to_neg_anf(clause: &Vec<(u64, u64)>) -> Vec<Vec<u64>> {
-        let mut clause_iterable_negated: Vec<Vec<u64>> = vec![vec![1]];
-        for m in clause {
-            clause_iterable_negated.push(vec![m.0, m.1]);
-        }
-
-        let mut neg_anf_simplified: Vec<Vec<u64>> = Vec::new();
-        for m in clause_iterable_negated
-            .iter()
-            .map(|v| v.iter().copied())
-            .multi_cartesian_product()
-        {
-            if !m.contains(&0) {
-                neg_anf_simplified.push(m.into_iter().filter(|v| *v != 1 as u64).collect());
-            }
-        }
-        neg_anf_simplified
-    }
-
-    // ========
-    // 4.i.
-    // ========
-    let mut t_prime_subset_of_t: Vec<Vec<FormattedElement>> = Vec::new();
-
-    for t_i in t_contained_by_clauses {
+    let t_prime_subset_of_t: Vec<Vec<FormattedElement>> = t_contained_by_clauses
+        .into_par_iter().filter(|t_i| {
         let c_1 = &t_i[0];
-        let anf_c_1: Vec<Vec<u64>> = cnf_to_neg_anf(&c_1.clause);
-        let chosen_monomial: Vec<u64> = anf_c_1[0].clone();
-        let m_prime_encoded_monomial: u128 = encode_bit_vector(chosen_monomial);
+        // 4.i. Pick any monomial from ANF(negation of c_1)
+        let anf_c_1: Vec<u128> = cnf_to_neg_anf(&c_1.clause);
+        let m_prime: u128 = anf_c_1[0].clone();
 
+        // 4.ii. S = union of vars in c_2...c_k, excluding vars in c_1
         let mut s_union_of_other_vars: u128 = 0b0;
         for c_i in &t_i[1..] {
             s_union_of_other_vars |= c_i.vars;
         }
-        s_union_of_other_vars = set_intersection(s_union_of_other_vars, !m_prime_encoded_monomial);
+        s_union_of_other_vars = s_union_of_other_vars & !c_1.vars;
 
+        // 4.iii. Sample random monomials supported on S
         let other_vars: Vec<u64> = decode_bit_vector(s_union_of_other_vars);
         let num_samples = MAX_SAMPLES.min(1usize << other_vars.len().min(63));
         let mut rng = rand::rng();
 
         let mut appearances = 0;
-        for i in 0..num_samples {
+        for _ in 0..num_samples {
             let mut m_i_bit_vector: u128 = 0;
             for &v in &other_vars {
                 if rng.random_bool(0.5) {
                     m_i_bit_vector |= 1u128 << (v - INDEX_0_OFFSET);
                 }
             }
-            let product = m_prime_encoded_monomial | m_i_bit_vector;
+            // 4.iv. Check if m_prime*m_i appears in the ciphertext
+            let product = m_prime | m_i_bit_vector;
             if unique_masks.contains(&product) {
-                appearances += 30;
+                appearances += 1;
             }
             if appearances >= SAMPLE_THRESHOLD {
-                t_prime_subset_of_t.push(t_i);
-                break;
+                return true;
             }
         }
+        false
+    })
+    .collect();
 
-        for t_prime_i in &t_prime_subset_of_t {
-            println!("===============");
-            for m in t_prime_i {
-                println!("{:b}", m.vars);
-            }
+    for t_prime_i in &t_prime_subset_of_t {
+        println!("===============");
+        for m in t_prime_i {
+            println!("{:b}", m.vars);
         }
     }
 
-    // ========
-    // 4.ii.
-    // ========
-
-    Ok(public_key_formatted)
+    Ok(t_prime_subset_of_t)
 }
 
 #[pymodule]
---
+++ src/validator/attacks/retrieve_rs/update.md
@@ -0,0 +1,9 @@
+1. Let S be a collection of subsets of the variables. Each subset s in S is formed by taking two clauses from the ciphertext and unioning the variables that they contain.
+2. Let S' be S but where we duplicate each s in S a few times as follows. For each variable in s, for each clause from the public key that shares a variable with s, union the variables in that clause with s.
+3. Let T be a collection of groups of clauses from the public key, i.e. a set of candidate beta groups. For each s in S', we take the set of all clauses that have all their variables contained within s to be a clause group and add this to T.
+
+4. For each t_i = {c_1, ..., c_k} in T, do the following:
+     i. Pick any monomial in ANF(negation of c_1). Let this monomial be m', which we represent as a set of variables. (It's also okay to pick the monomial "1" if present, in which case the set m' would be empty.)
+     ii. Let S be the union of variables involved in c_2, ..., c_k, but excluding the variables already present in c_1.
+     iii. Pick 100 uniformly random monomials m_1, ..., m_100 supported on S. As with m', represent these as sets of variables.
+     iv. Count how many of the monomials m'm_1, ..., m'm_100 actually appear in the ciphertext. If the count is at least 30, add t_i to the new set T'. Otherwise, don't add t_i.
